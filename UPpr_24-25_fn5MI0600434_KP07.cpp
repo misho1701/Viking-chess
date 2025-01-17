419d5fc8043696d4cb1228671664a6b2a86fc90d
misho1701 <115742050+misho1701@users.noreply.github.com>
2025-01-17 17:11:08 +0200

    Add files via upload

--- UPpr_24-25_fn5MI0600434_KP07.cpp
+++ UPpr_24-25_fn5MI0600434_KP07.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
 
 const int MAX_BOARD_SIZE = 13;
 
-
 char board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
 int boardSize;
-
 
 int startXHistory[100], startYHistory[100];
 int endXHistory[100], endYHistory[100];
@@ -16,9 +12,7 @@ int capturedXHistory[100], capturedYHistory[100];
 char capturedPieceHistory[100];
 int moveCount = 0;
 
-
 int currentPlayer = 1;
-
 
 void initializeBoard(int size);
 void drawBoard();
@@ -27,26 +21,26 @@ void checkCapture(int x, int y, int& capturedX, int& capturedY, char& capturedPi
 bool checkVictory();
 void makeMove(int startX, int startY, int endX, int endY);
 void undoMove();
-
-
 void showHelp();
 void showInfo();
 void displayMenu();
 
+int parseNumber(const char* str);
 
 int main() {
-    string menuChoice;
+    char menuChoice[10];
     bool gameRunning = true;
 
     while (gameRunning) {
         displayMenu();
         cout << "Enter your choice: ";
-        cin >> menuChoice;
+        cin.getline(menuChoice, 10);
 
-        if (menuChoice == "1") {
-            
+        if (menuChoice[0] == '1' && menuChoice[1] == '\0') {
             cout << "Enter board size (7 to 13): ";
-            cin >> boardSize;
+            char sizeInput[10];
+            cin.getline(sizeInput, 10);
+            boardSize = parseNumber(sizeInput);
 
             if (boardSize < 7 || boardSize > 13) {
                 cout << "Invalid board size!" << endl;
@@ -58,24 +52,35 @@ int main() {
             while (true) {
                 drawBoard();
                 cout << "Player " << (currentPlayer == 1 ? "Attackers" : "Defenders") << ", enter a command: ";
-                string command;
-                cin >> command;
+                char command[50];
+                cin.getline(command, 50);
 
-                if (command == "move") {
+                if (command[0] == 'm' && command[1] == 'o' && command[2] == 'v' && command[3] == 'e') {
                     int startX, startY, endX, endY;
-                    cin >> startX >> startY >> endX >> endY;
+                    char* ptr = command + 5;
+                    startX = parseNumber(ptr);
+                    while (*ptr && *ptr != ' ') ptr++;
+                    if (*ptr) ptr++;
+                    startY = parseNumber(ptr);
+                    while (*ptr && *ptr != ' ') ptr++;
+                    if (*ptr) ptr++;
+                    endX = parseNumber(ptr);
+                    while (*ptr && *ptr != ' ') ptr++;
+                    if (*ptr) ptr++;
+                    endY = parseNumber(ptr);
+
                     makeMove(startX, startY, endX, endY);
                 }
-                else if (command == "back") {
+                else if (command[0] == 'b' && command[1] == 'a' && command[2] == 'c' && command[3] == 'k') {
                     undoMove();
                 }
-                else if (command == "help") {
+                else if (command[0] == 'h' && command[1] == 'e' && command[2] == 'l' && command[3] == 'p') {
                     showHelp();
                 }
-                else if (command == "info") {
+                else if (command[0] == 'i' && command[1] == 'n' && command[2] == 'f' && command[3] == 'o') {
                     showInfo();
                 }
-                else if (command == "quit") {
+                else if (command[0] == 'q' && command[1] == 'u' && command[2] == 'i' && command[3] == 't') {
                     break;
                 }
                 else {
@@ -83,8 +88,7 @@ int main() {
                 }
             }
         }
-        else if (menuChoice == "2") {
-            
+        else if (menuChoice[0] == '2' && menuChoice[1] == '\0') {
             cout << "Thank you for playing Viking Chess! Goodbye!" << endl;
             gameRunning = false;
         }
@@ -96,22 +100,18 @@ int main() {
     return 0;
 }
 
-
 void initializeBoard(int size) {
     boardSize = size;
     int center = size / 2;
 
-    
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             board[i][j] = '.';
         }
     }
 
-    
     board[center][center] = 'K';
 
-    
     board[center - 1][center] = 'D';
     board[center + 1][center] = 'D';
     board[center][center - 1] = 'D';
@@ -124,7 +124,6 @@ void initializeBoard(int size) {
         board[center][center + 2] = 'D';
     }
 
-    
     for (int i = 2; i < size - 2; i++) {
         board[0][i] = 'A';
         board[size - 1][i] = 'A';
@@ -137,7 +136,6 @@ void initializeBoard(int size) {
     board[1][center] = 'A';
     board[size - 2][center] = 'A';
 }
-
 
 void drawBoard() {
     cout << "  ";
@@ -152,14 +150,12 @@ void drawBoard() {
     }
 }
 
-
 bool isValidMove(int startX, int startY, int endX, int endY) {
     if (startX < 0 || startX >= boardSize || startY < 0 || startY >= boardSize ||
         endX < 0 || endX >= boardSize || endY < 0 || endY >= boardSize) {
         return false;
     }
 
-    
     if ((endX == boardSize / 2 && endY == boardSize / 2) ||
         (endX == 0 && endY == 0) ||
         (endX == 0 && endY == boardSize - 1) ||
@@ -168,23 +164,19 @@ bool isValidMove(int startX, int startY, int endX, int endY) {
         return false;
     }
 
-    
     if (board[startX][startY] == '.' || board[endX][endY] != '.') {
         return false;
     }
 
-    
     if (startX != endX && startY != endY) {
         return false;
     }
 
-    
     if (startX == endX) {
         for (int i = min(startY, endY) + 1; i < max(startY, endY); i++) {
             if (board[startX][i] != '.') return false;
         }
     }
-    
     else {
         for (int i = min(startX, endX) + 1; i < max(startX, endX); i++) {
             if (board[i][startY] != '.') return false;
@@ -194,13 +186,12 @@ bool isValidMove(int startX, int startY, int endX, int endY) {
     return true;
 }
 
-
 void checkCapture(int x, int y, int& capturedX, int& capturedY, char& capturedPiece) {
     const int dx[] = { -1, 1, 0, 0 };
     const int dy[] = { 0, 0, -1, 1 };
     char opponent = (currentPlayer == 1 ? 'D' : 'A');
 
-    capturedX = -1; 
+    capturedX = -1;
     capturedY = -1;
     capturedPiece = '.';
 
@@ -220,7 +211,6 @@ void checkCapture(int x, int y, int& capturedX, int& capturedY, char& capturedPi
     }
 }
 
-
 bool checkVictory() {
     const int corners[4][2] = { {0, 0}, {0, boardSize - 1}, {boardSize - 1, 0}, {boardSize - 1, boardSize - 1} };
     for (int i = 0; i < 4; i++) {
@@ -231,7 +221,6 @@ bool checkVictory() {
         }
     }
 
-    
     for (int i = 0; i < boardSize; i++) {
         for (int j = 0; j < boardSize; j++) {
             if (board[i][j] == 'K') {
@@ -264,7 +253,6 @@ bool checkVictory() {
     return false;
 }
 
-
 void makeMove(int startX, int startY, int endX, int endY) {
     if (!isValidMove(startX, startY, endX, endY)) {
         cout << "Invalid move!" << endl;
@@ -279,7 +267,6 @@ void makeMove(int startX, int startY, int endX, int endY) {
     char capturedPiece;
     checkCapture(endX, endY, capturedX, capturedY, capturedPiece);
 
-    
     startXHistory[moveCount] = startX;
     startYHistory[moveCount] = startY;
     endXHistory[moveCount] = endX;
@@ -292,7 +279,6 @@ void makeMove(int startX, int startY, int endX, int endY) {
     if (checkVictory()) exit(0);
     currentPlayer = 3 - currentPlayer;
 }
-
 
 void undoMove() {
     if (moveCount == 0) {
@@ -319,7 +305,6 @@ void undoMove() {
     currentPlayer = 3 - currentPlayer;
 }
 
-
 void showHelp() {
     cout << "Commands:\n";
     cout << "  move x1 y1 x2 y2 - move a piece from (x1, y1) to (x2, y2)\n";
@@ -329,14 +314,22 @@ void showHelp() {
     cout << "  quit - quit the game\n";
 }
 
-
 void showInfo() {
     cout << "Player " << (currentPlayer == 1 ? "Attackers" : "Defenders") << "'s turn.\n";
 }
-
 
 void displayMenu() {
     cout << "\nViking Chess - Tafl\n";
     cout << "1. Start a New Game\n";
     cout << "2. Quit\n";
 }
+
+int parseNumber(const char* str) {
+    int result = 0;
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    return result;
+}
+
